b5056ff58d4ff43a890ca10b420898803854621a
hikanaka <roundupusa02@gmail.com>
2018-11-18 09:41:52 +0900

    3テーブル分の割り勘/但し1テーブル分だけ

---
+++ app.py
@@ -0,0 +1,7 @@
+# table1: amount = 1500 , number_of_people = 3
+amount = 1500
+number_of_people = 3
+
+print(amount / number_of_people)
+
+print(f"一人あたり:{amount // number_of_people}円,端数:{amount % number_of_people}")
