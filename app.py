f2b4a31e0ff10ca9e5189df798d9962ebe8129af
hikanaka <roundupusa02@gmail.com>
2018-11-18 10:48:47 +0900

    First commmit

--- app.py
+++
@@ -1,7 +0,0 @@
-# table1: amount = 1500 , number_of_people = 3
-amount = 1500
-number_of_people = 3
-
-print(amount / number_of_people)
-
-print(f"一人あたり:{amount // number_of_people}円,端数:{amount % number_of_people}")
---
+++ app2.py
@@ -0,0 +1,4 @@
+# 割り算計算したい 7000円で6人
+from warikan2 import calculate_warikan
+
+print(calculate_warikan(amount=7000, number_of_people=6))
